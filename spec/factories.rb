abae946bc9e2434e1d522247311f0553a4ca7ead
Nicolay Hvidsten <nico.hvi@gmail.com>
2014-06-13 14:29:06 +0200

    updated to rspec 3.x

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
-  
+
   factory :user do
     username 'omar'
     email 'omar@baltimore.org'
 
     factory :invalid_user do
-      email: ''
+      email ''
     end
   end
 
